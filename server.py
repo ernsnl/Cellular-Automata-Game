781bbcacd809d15d25ebc2c77b3eb79270ce5c82
Eren Senel <ernsnl@gmail.com>
2017-03-07 20:44:16 -0500

    Multiple Conn

--- server.py
+++ server.py
@@ -26,10 +26,11 @@ if s is None:
     print 'could not open socket'
     sys.exit(1)
 
-conn, addr = s.accept()
-print 'Connected by', addr
 while 1:
-    data = conn.recv(1024)
-    if not data: break
-    conn.send(data)
+    conn, addr = s.accept()
+    print 'Connected by', addr
+    while 1:
+        data = conn.recv(1024)
+        if not data: break
+        conn.send(data)
 conn.close()
